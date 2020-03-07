36579146a9cf1f7758044336f4965e2bf8c780ff
paul.martin <paul.y.martin@johnlewis.co.uk>
2020-03-07 16:32:42 +0000

    Processor upgrade

--- src/main/kotlin/Laptop.kt
+++ src/main/kotlin/Laptop.kt
@@ -1,4 +1,19 @@
-class Laptop {
-    val description = "A laptop"
-    val price = 40000
+interface Laptop {
+    val description: String
+    val price: Int
+}
+
+class SimpleLaptop : Laptop {
+    override val description = "A laptop"
+    override val price = 40000
+}
+
+abstract class LaptopDecorator(private val decoratedLaptop: Laptop) : Laptop {
+    override val description = decoratedLaptop.description
+    override val price = decoratedLaptop.price
+}
+
+class ProcessorUpgrade(private val laptop: Laptop) : LaptopDecorator(laptop) {
+    override val description = super.description + ", 1.21 GigaWatts processor"
+    override val price = super.price + 5000
 }
--- src/test/kotlin/LaptopTest.kt
+++ src/test/kotlin/LaptopTest.kt
@@ -1,12 +1,19 @@
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 
-class LaptopTest : StringSpec ({
+class LaptopTest : StringSpec({
     "a laptop should have a description" {
-        Laptop().description shouldBe "A laptop"
+        SimpleLaptop().description shouldBe "A laptop"
     }
 
     "a laptop should have a price" {
-        Laptop().price shouldBe 40000
+        SimpleLaptop().price shouldBe 40000
+    }
+
+    "a laptop can have its processor upgraded" {
+        val laptop = SimpleLaptop()
+        val upgradedLaptop = ProcessorUpgrade(laptop)
+        upgradedLaptop.description shouldBe "A laptop, 1.21 GigaWatts processor"
+        upgradedLaptop.price shouldBe 45000
     }
 })
