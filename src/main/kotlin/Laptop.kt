d00f3212806de238cb5887c74126308e0bb44946
paul.martin <paul.y.martin@johnlewis.co.uk>
2020-03-08 18:35:57 +0000

    Add all upgrades

--- src/main/kotlin/Laptop.kt
+++ src/main/kotlin/Laptop.kt
@@ -5,7 +5,7 @@ interface Laptop {
 
 class SimpleLaptop : Laptop {
     private val itemPrice = 399.99
-    override val description = "Simple laptop @ £$itemPrice" // TODO pad zeroes! need test?!
+    override val description = "Simple laptop @ £${itemPrice.padZeros()}"
     override val price = itemPrice
 }
 
@@ -23,5 +23,29 @@ class ProcessorUpgrade(laptop: Laptop) : LaptopDecorator(laptop) {
 class MemoryUpgrade(laptop: Laptop) : LaptopDecorator(laptop) {
     private val upgradePrice = 50.0
     override val description = super.description + ", extra 16Gb RAM @ £${upgradePrice.padZeros()}"
+    override val price = super.price + upgradePrice
+}
+
+class StorageUpgrade(laptop: Laptop) : LaptopDecorator(laptop) {
+    private val upgradePrice = 99.98
+    override val description = super.description + ", 1Tb SSD @ £${upgradePrice.padZeros()}"
+    override val price = super.price + upgradePrice
+}
+
+class GraphicsUpgrade(laptop: Laptop) : LaptopDecorator(laptop) {
+    private val upgradePrice = 47.77
+    override val description = super.description + ", Technicolor @ £${upgradePrice.padZeros()}"
+    override val price = super.price + upgradePrice
+}
+
+class BatteryUpgrade(laptop: Laptop) : LaptopDecorator(laptop) {
+    private val upgradePrice = 3.99
+    override val description = super.description + ", 4 * AAA included @ £${upgradePrice.padZeros()}"
+    override val price = super.price + upgradePrice
+}
+
+class ShinyCaseUpgrade(laptop: Laptop) : LaptopDecorator(laptop) {
+    private val upgradePrice = 300.00
+    override val description = super.description + ", Teh Shinies @ £${upgradePrice.padZeros()}"
     override val price = super.price + upgradePrice
 }
--- src/test/kotlin/LaptopTest.kt
+++ src/test/kotlin/LaptopTest.kt
@@ -1,5 +1,7 @@
+import io.kotlintest.matchers.string.shouldInclude
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
+import org.eclipse.jgit.lib.Ref
 
 class LaptopTest : StringSpec({
     "a laptop should have a description" {
@@ -13,20 +15,62 @@ class LaptopTest : StringSpec({
     "a laptop can have its processor upgraded" {
         val laptop = SimpleLaptop()
         val upgradedLaptop = ProcessorUpgrade(laptop)
-        upgradedLaptop.description shouldBe "Simple laptop @ £399.99, 1.21 GigaWatts processor @ £80.00"
+        upgradedLaptop.description shouldInclude "Simple laptop @ £399.99"
+        upgradedLaptop.description shouldInclude "1.21 GigaWatts processor @ £80.00"
         upgradedLaptop.price shouldBe 479.99
     }
 
     "a laptop can have its memory upgraded" {
         val laptop = SimpleLaptop()
         val upgradedLaptop = MemoryUpgrade(laptop)
-        upgradedLaptop.description shouldBe "Simple laptop @ £399.99, extra 16Gb RAM @ £50.00"
+        upgradedLaptop.description shouldInclude "Simple laptop @ £399.99"
+        upgradedLaptop.description shouldInclude "extra 16Gb RAM @ £50.00"
         upgradedLaptop.price shouldBe 449.99
     }
 
     "a laptop can have its memory and processor upgraded" {
         val upgradedLaptop = MemoryUpgrade(ProcessorUpgrade(SimpleLaptop()))
-        upgradedLaptop.description shouldBe "Simple laptop @ £399.99, 1.21 GigaWatts processor @ £80.00, extra 16Gb RAM @ £50.00"
+        upgradedLaptop.description shouldInclude "Simple laptop @ £399.99"
+        upgradedLaptop.description shouldInclude "1.21 GigaWatts processor @ £80.00"
+        upgradedLaptop.description shouldInclude "extra 16Gb RAM @ £50.00"
         upgradedLaptop.price shouldBe 529.99
+    }
+
+    "a laptop can have its storage upgraded" {
+        val laptop = SimpleLaptop()
+        val upgradedLaptop = StorageUpgrade(laptop)
+        upgradedLaptop.description shouldInclude "Simple laptop @ £399.99"
+        upgradedLaptop.description shouldInclude "1Tb SSD @ £99.98"
+        upgradedLaptop.price shouldBe 499.97
+    }
+
+    "a laptop can have its graphics upgraded" {
+        val laptop = SimpleLaptop()
+        val upgradedLaptop = GraphicsUpgrade(laptop)
+        upgradedLaptop.description shouldInclude "Simple laptop @ £399.99"
+        upgradedLaptop.description shouldInclude "Technicolor @ £47.77"
+        upgradedLaptop.price shouldBe 447.76
+    }
+
+    "a laptop can have its battery upgraded" {
+        val laptop = SimpleLaptop()
+        val upgradedLaptop = BatteryUpgrade(laptop)
+        upgradedLaptop.description shouldInclude "Simple laptop @ £399.99"
+        upgradedLaptop.description shouldInclude "4 * AAA included @ £3.99"
+        upgradedLaptop.price shouldBe 403.98
+    }
+
+    "a laptop can have its case upgraded" {
+        val laptop = SimpleLaptop()
+        val upgradedLaptop = ShinyCaseUpgrade(laptop)
+        upgradedLaptop.description shouldInclude "Simple laptop @ £399.99"
+        upgradedLaptop.description shouldInclude "Teh Shinies @ £300.00"
+        upgradedLaptop.price shouldBe 699.99
+    }
+
+    "a laptop can have everything upgraded" {
+        val dreamMachine = ShinyCaseUpgrade(BatteryUpgrade(GraphicsUpgrade(StorageUpgrade(MemoryUpgrade(ProcessorUpgrade(SimpleLaptop()))))))
+        dreamMachine.price shouldBe 981.73
+        dreamMachine.description shouldBe "Simple laptop @ £399.99, 1.21 GigaWatts processor @ £80.00, extra 16Gb RAM @ £50.00, 1Tb SSD @ £99.98, Technicolor @ £47.77, 4 * AAA included @ £3.99, Teh Shinies @ £300.00"
     }
 })
